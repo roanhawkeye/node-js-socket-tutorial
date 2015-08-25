d10f3a0f18adb364164e3ebeebb4678e362ada98
Daniel Martinez <daniel.martinez@mexicoder.com.mx>
2015-08-25 17:26:20 -0500

    Add lib redis module

---
+++ models/badges.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var redis = require('../lib/redis');
+
+/*
+* Save badges to database
+* @Param {Array} badges
+* @Param {Function} callback
+*/
+exports.save = function(badges, callback){
+
+};
