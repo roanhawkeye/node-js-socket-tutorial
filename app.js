f73d91015ddbd4876272ed8fc03bf9deb917aed6
Daniel Martinez <daniel.martinez@mexicoder.com.mx>
2015-08-26 14:59:32 -0500

    Add get endpoint to /badges url to app.js

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ app.set('port', process.env.PORT || 8000);
 
 app.post('/', badges.save, badges.send);
 
+app.get('/badges', badges.get);
+
 app.listen(app.get('port'), function(){
 	console.log('Express server listening on port ' + app.get('port'));
 });
