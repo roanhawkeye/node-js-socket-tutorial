3b3d4f32a36fd3c458d6cdb271f35bc3fe72f973
Daniel Martinez <daniel.martinez@mexicoder.com.mx>
2015-08-26 15:13:50 -0500

    Add model method get functionality

--- models/badges.js
+++ models/badges.js
@@ -36,3 +36,14 @@ exports.send = function(badges, callback){
 	badges.forEach(broadcast.send);
 	callback(null, null);
 };
+
+/**
+* Get badges from redis
+* @Param {Function}	callback 
+*/
+exports.get = function(callback){
+	redis.lrange('badges', 0, -1, function(err, data){
+		if (err) return callback(err, null);
+		callback(null, data.map(JSON.parse));
+	});
+};
