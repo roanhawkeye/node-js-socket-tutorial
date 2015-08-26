5bb08a2f5f349dd16ee62c6709ef85c8ff7ef307
Daniel Martinez <daniel.martinez@mexicoder.com.mx>
2015-08-26 13:00:19 -0500

    Add log when listen to port and set app variable port

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ var badges = require('./controllers/badges');
 
 app.use(express.json());
 
+app.set('port', process.env.PORT || 8000);
+
 app.post('/', badges.save, badges.send, function(req, res){
 	res.send('\nDone\n\n');
 });
 
-app.listen(8000);
+app.listen(app.get('port'), function(){
+	console.log('Express server listening on port ' + app.get('port'));
+});
